1dddc77f719fd28682698bb9999dd41b3c6d10d6
julfinch <julfinch@gmail.com>
2022-09-14 02:39:07 +0800

    mobile responsiveness Home page

--- src/components/Featured/index.jsx
+++ src/components/Featured/index.jsx
@@ -7,8 +7,10 @@ export default function Featured() {
   return (
     <section className="featured-section" data-scroll-section>
       <div className="section-5" >
+        <div className="stack-title">
         <motion.h2 initial={{ y: 100, opacity: 0 }} whileInView={{ y: 0, opacity: 1, transition: { type: "easeOut", duration: 1} }} viewport={{ once: true }}>Tech Stack</motion.h2>
         <motion.span initial={{ y: 100, opacity: 0 }} whileInView={{ y: 0, opacity: 1, transition: { type: "easeOut", duration: 1} }} viewport={{ once: true }}>To bring the idea to life, these are the techologies, skills, and tools that I heavily rely upon.</motion.span>
+        </div>
         <div className="stack-container" >
         {icons.map((item, index) => (
           <>
